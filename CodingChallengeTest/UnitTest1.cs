44602739a3aaa252d3596bbe27e0ead9bd4629ed
Ollie Whiteman <Ollie.Whiteman@kmart.com.au>
2022-03-31 10:55:08 +1100

    Function passing first test

--- CodingChallengeTest/UnitTest1.cs
+++ CodingChallengeTest/UnitTest1.cs
@@ -1,14 +1,21 @@
 using System;
 using Xunit;
+using CodingChallenge; 
 
 namespace CodingChallengeTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        Program method = new Program();
 
+        [Fact]
+        public void TestCase01()
+        {
+            string input = "6 1 5 9 2";
+            string output = method.FindSequence(input);
+            string expectedOutput = "1 5 9";
+
+            Assert.Equal(expectedOutput, output);
         }
     }
 }
